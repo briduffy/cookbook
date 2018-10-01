bd6c44d7a2d40835023affdba49a72dc84600419
Brianne Duffy <duffymbrianne@gmail.com>
2018-10-01 13:30:51 -0600

    components and controllers

---
+++ app/controllers/api/recipes_controller.rb
@@ -0,0 +1,47 @@
+class Api::RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :update, :destroy]
+
+  def index
+    render json: Recipe.all
+  end
+
+  def show
+    render json: {
+      id: @recipe.id,
+      name: @recipe.name,
+      description: @recipe.description,
+      ingredients: @recipe.recipe_ingredients
+    }
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+
+    if recipe.save
+      render json: recipe
+    else
+      render_error(recipe)
+    end
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      render json: @recipe
+    else
+      render_error(@recipe)
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+  end
+
+  private
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    def recipe_params
+      params.require(:recipe).permit(:name, :description)
+    end
+end
--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react'
+import {
+  Switch,
+  Route,
+  Link,
+  withRouter,
+} from 'react-router-dom'
+import { Menu } from 'semantic-ui-react'
+import AddIngredient from './components/AddIngredient'
+import AddRecipe from './components/AddRecipe'
+import Recipe from './components/Recipe'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const links = [
+  'recipes',
+  'ingredients',
+  'add_ingredients',
+  'add_recipe',
+]
+const App = ({ location }) => (
+  <Fragment>
+    <Menu>
+      { links.map ( link => 
+        <Link key={link} to={`/${link}`}>
+          <Menu.Item
+            name={link}
+            active={ `/${link}` === location.pathhome}
+          />
+        </Link>
+        )}
+    </Menu>
+    <Switch>
+      <Route exact path="/add_ingredients" component={AddIngredient} />
+      <Route exact path="/add_recipe" component={AddRecipe} />
+      <Route exact path="/recipes/:id" component={Recipe} />
+    </Switch>
+  </Fragment>
+)
 
-export default App;
+export default withRouter(App)
