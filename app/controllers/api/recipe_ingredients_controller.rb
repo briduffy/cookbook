c8d50b6535dbcde93e72c8a00d885ad5f069767b
Brianne Duffy <duffymbrianne@gmail.com>
2018-10-01 10:19:13 -0600

    completed controllers

--- app/controllers/api/recipe_ingredients_controller.rb
+++ app/controllers/api/recipe_ingredients_controller.rb
@@ -1,2 +1,25 @@
 class Api::RecipeIngredientsController < ApplicationController
+  def new
+    ingredients = Ingredient.all.map { |i| {name: i.name, i: id.id } }
+    render json: ingredient
+  end
+
+  def create
+    recipe_ingredient = RecipeIngredient.new(rec_ing_params)
+
+    if recipe_ingredient.save
+      render json: recipe_ingredient
+    else
+      render_error(recipe_ingredient)
+    end
+  end
+
+  def destroy
+    RecipeIngredient.find(params[:id]).destroy
+  end
+
+  private
+  def rec_ing_params
+    params.require(:recipe_ingredient).permit(:recipe_id, :ingredient_id, :amount)
+  end
 end
